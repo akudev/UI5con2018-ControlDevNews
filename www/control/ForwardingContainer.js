e1118fe1a877a50c4445debd81684ba7da523f33
Andreas Kunz <andreas.kunz@sap.com>
2018-06-11 16:04:51 +0200

    Aggregation Forwarding examples

---
+++ www/control/ForwardingContainer.js
@@ -0,0 +1,21 @@
+sap.ui.define(
+		['jquery.sap.global', 'sap/ui/core/XMLComposite'],
+		function (jQuery, XMLComposite) {
+	"use strict";
+	
+	var oForwardingContainer = XMLComposite.extend("control.ForwardingContainer", {
+		metadata: {
+			aggregations: {
+				content: {
+					type: "sap.ui.core.Control",
+					multiple: true,
+					forwarding: {
+					   idSuffix: "--panel", // ID given in XML fragment is prefixed with double-dash!
+					   aggregation: "content"
+					}
+				}
+			}
+		}
+	});
+	return oForwardingContainer;
+}, /* bExport= */ true);
